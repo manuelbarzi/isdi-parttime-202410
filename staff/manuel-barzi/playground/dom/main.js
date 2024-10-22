5cfbfb1fd459189bc913f8a1d83d5a3f56b61e87
Manuel Barzi <manuelbarzi@gmail.com>
2024-10-22 21:34:34 +0200

    add dom folder with base files #1

---
+++ staff/manuel-barzi/playground/dom/main.js
@@ -0,0 +1,42 @@
+console.log('Hello, DOM!')
+
+function printDOMTree() {
+    // TODO extract and print dom tree from current document html (HINT use recursion)
+}
+
+printDOMTree()
+
+/*
+html
+    head
+        meta
+        meta
+        title
+    body
+        h1
+        p
+        h2
+        ul
+            li
+            li
+            li
+        h2
+        table
+            tr
+                th
+                th
+                th
+            tr
+                td
+                td
+                td
+            tr
+                td
+                td
+                td
+            tr
+                td
+                td
+                td
+        script
+*/
