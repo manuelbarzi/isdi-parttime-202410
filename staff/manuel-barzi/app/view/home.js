f4fed8d5cc149ac27990f3e5c5101f377d930951
Manuel Barzi <manuelbarzi@gmail.com>
2024-10-31 21:43:02 +0100

    add create post functionality and posts refreshing #23

--- staff/manuel-barzi/app/view/home.js
+++ staff/manuel-barzi/app/view/home.js
@@ -28,6 +28,15 @@ homeLogoutButton.onclick = function () {
     body.appendChild(loginView)
 }
 
+var homeAddPostButton = document.createElement('button')
+homeAddPostButton.innerText = '+'
+homeView.appendChild(homeAddPostButton)
+
+homeAddPostButton.onclick = function () {
+    homeView.removeChild(homePosts)
+    homeView.appendChild(homeCreatePost)
+}
+
 var homePosts = document.createElement('section')
 homeView.appendChild(homePosts)
 
@@ -48,12 +57,91 @@ if (isUserLoggedIn()) {
         postImage.src = post.image
         homePost.appendChild(postImage)
 
-        var postCaption = document.createElement('p')
-        postCaption.innerText = post.text
-        homePost.appendChild(postCaption)
+        var postText = document.createElement('p')
+        postText.innerText = post.text
+        homePost.appendChild(postText)
 
         var postDate = document.createElement('time')
         postDate.innerText = post.date
         homePost.appendChild(postDate)
     })
+}
+
+var homeCreatePost = document.createElement('section')
+
+var homeCreatePostTitle = document.createElement('h3')
+homeCreatePostTitle.innerText = 'Create Post'
+homeCreatePost.appendChild(homeCreatePostTitle)
+
+var homeCreatePostForm = document.createElement('form')
+homeCreatePost.appendChild(homeCreatePostForm)
+
+var homeCreatePostImageLabel = document.createElement('label')
+homeCreatePostImageLabel.innerText = 'Image'
+homeCreatePostImageLabel.htmlFor = 'image'
+homeCreatePostForm.appendChild(homeCreatePostImageLabel)
+
+var homeCreatePostImageInput = document.createElement('input')
+homeCreatePostImageInput.type = 'url'
+homeCreatePostImageInput.id = 'image'
+homeCreatePostForm.appendChild(homeCreatePostImageInput)
+
+var homeCreatePostTextLabel = document.createElement('label')
+homeCreatePostTextLabel.innerText = 'Text'
+homeCreatePostTextLabel.htmlFor = 'text'
+homeCreatePostForm.appendChild(homeCreatePostTextLabel)
+
+var homeCreatePostTextInput = document.createElement('input')
+homeCreatePostTextInput.type = 'text'
+homeCreatePostTextInput.id = 'text'
+homeCreatePostForm.appendChild(homeCreatePostTextInput)
+
+var homeCreatePostSubmitButton = document.createElement('button')
+homeCreatePostSubmitButton.innerText = 'Create'
+homeCreatePostSubmitButton.type = 'submit'
+homeCreatePostForm.appendChild(homeCreatePostSubmitButton)
+
+homeCreatePostForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    var image = homeCreatePostImageInput.value
+    var text = homeCreatePostTextInput.value
+
+    try {
+        createPost(image, text)
+
+        homeCreatePostForm.reset()
+
+        homeView.removeChild(homeCreatePost)
+        homeView.appendChild(homePosts)
+
+        var posts = getPosts()
+
+        homePosts.innerHTML = ''
+
+        posts.forEach(function (post) {
+            var homePost = document.createElement('article')
+            homePosts.appendChild(homePost)
+
+            var postAuthor = document.createElement('h3')
+            postAuthor.innerText = post.author
+            homePost.appendChild(postAuthor)
+
+            var postImage = document.createElement('img')
+            postImage.src = post.image
+            homePost.appendChild(postImage)
+
+            var postText = document.createElement('p')
+            postText.innerText = post.text
+            homePost.appendChild(postText)
+
+            var postDate = document.createElement('time')
+            postDate.innerText = post.date
+            homePost.appendChild(postDate)
+        })
+    } catch (error) {
+        alert(error.message)
+
+        console.error(error)
+    }
 }
