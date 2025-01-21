0928c82005e05488d89a1c955858372551733f20
manuelbarzi <manuelbarzi@gmail.com>
2025-01-21 21:37:01 +0100

    add mongoose in api for data modeling; #23

--- staff/manuel-barzi/product/api/data/populate.js
+++ staff/manuel-barzi/product/api/data/populate.js
@@ -1,17 +1,37 @@
-import mongodb from 'mongodb'
+import mongoose from 'mongoose'
 
-const { MongoClient } = mongodb
+const { Schema, model } = mongoose
 
-const client = new MongoClient('mongodb://localhost:27017')
+const user = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
 
-client.connect()
-    .then(connection => {
-        const db = connection.db('test')
+const User = model('User', user)
 
-        const users = db.collection('users')
 
-        users.insertOne({ name: 'Pepito Grillo', email: 'pepito@grillo.com', username: 'pepitogrillo', password: '123123123' })
-            .then(result => console.log(result))
+mongoose.connect('mongodb://localhost:27017/test')
+    .then(() => {
+        const pepito = new User({ name: 'Pepito Grillo', email: 'pepito@grillo.com', username: 'pepitogrillo', password: '123123123' })
+
+        pepito.save()
+            .then(() => console.log('user saved'))
             .catch(error => console.error(error))
     })
     .catch(error => console.error(error))
