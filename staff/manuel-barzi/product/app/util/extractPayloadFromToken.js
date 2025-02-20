2aa4b338523a731b222de9aa2cf937dec07d2bca
manuelbarzi <manuelbarzi@gmail.com>
2025-02-20 21:29:54 +0100

    remove var from util; add no-var rule in eslint #23

--- staff/manuel-barzi/product/app/util/extractPayloadFromToken.js
+++ staff/manuel-barzi/product/app/util/extractPayloadFromToken.js
@@ -1,10 +1,10 @@
 function extractPayloadFromToken(token) {
-    var firstDotIndex = token.indexOf('.')
-    var lastDotIndex = token.lastIndexOf('.')
+    const firstDotIndex = token.indexOf('.')
+    const lastDotIndex = token.lastIndexOf('.')
 
-    var payloadB64 = token.slice(firstDotIndex + 1, lastDotIndex)
-    var payloadJSON = atob(payloadB64)
-    var payload = JSON.parse(payloadJSON)
+    const payloadB64 = token.slice(firstDotIndex + 1, lastDotIndex)
+    const payloadJSON = atob(payloadB64)
+    const payload = JSON.parse(payloadJSON)
 
     return payload
 }
